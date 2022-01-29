ea60cc9e063d08a09aab613a34646ea80e78551e
LovellOduor <lovelloduor123@gmail.com>
2022-01-29 17:48:46 +0300

    First Commit

---
+++ 05_TFLite_Image_Classification/CMakeFiles/TFLiteImageClassification.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TFLiteImageClassification.dir/main.cpp.o"
+  "TFLiteImageClassification"
+  "TFLiteImageClassification.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TFLiteImageClassification.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
